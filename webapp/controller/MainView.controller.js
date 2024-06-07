8eb2bcb36e875421183b888a45c5d2714770bfaa
OliCasiano <oliversanchezcasiano@gmail.com>
2024-06-07 08:50:24 +0000

    Add SelectionScreenMenu in model folder

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -6,7 +6,10 @@ function (Controller) {
 
     return Controller.extend("capgemini.invoice.controller.MainView", {
         onInit: function () {
-
+            const oJSONModel = new sap.ui.model.json.oJSONModel();
+            const oView = this.getView();
+            oJSONModel.loadData("./model/SelectionScreenMenu.json");
+            oView.setModel(oJSONModel, "selectionScreen");
         }
     });
 });
