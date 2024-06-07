475360e31705bc65aa769ab4bcecd8cc2806da24
OliCasiano <oliversanchezcasiano@gmail.com>
2024-06-07 10:51:45 +0000

    Add Button and Event to clear input and list

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,15 +1,25 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("capgemini.invoice.controller.MainView", {
         onInit: function () {
-            const oJSONModel = new sap.ui.model.json.oJSONModel();
-            const oView = this.getView();
+            var oJSONModel = new JSONModel();
+            var oView = this.getView();
             oJSONModel.loadData("./model/SelectionScreenMenu.json");
             oView.setModel(oJSONModel, "selectionScreen");
+        },
+
+        onFilter: function (oEvent) {
+
+        },
+
+        onClearFilter: function () {
+            const oModelSelScreen = this.getView().getModel("selectionScreen");
+            oModelSelScreen.setProperty("/ShipName", "");
+            oModelSelScreen.setProperty("/CountryKey", "");
         }
     });
 });
